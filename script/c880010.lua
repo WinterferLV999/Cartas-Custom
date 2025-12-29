e8480fd2a3d472674d086e692575d2a7d2a344b6
Winterfer <feydersonrodriguez13@gmail.com>
2025-12-29 18:33:52 -0400

    la siguiente carta ha sido añadida: - Hundred eyes Darknightopia

---
+++ script/c880010.lua
@@ -0,0 +1,100 @@
+
+local s,id=GetID()
+function s.initial_effect(c)
+	--Synchro Summon Procedure
+	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_DARK),1,1,Synchro.NonTunerEx(s.matfilter),1,1)
+	--copy
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_ADJUST)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--Special Summon 1 monster from either GY to your opponent's field in Defense Position
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetHintTiming(0,TIMING_MAIN_END|TIMINGS_CHECK_MONSTER_E)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+end
+function s.matfilter(c,val,scard,sumtype,tp)
+	return c:IsRace(RACE_DRAGON,scard,sumtype,tp) and c:IsAttribute(ATTRIBUTE_DARK,scard,sumtype,tp) and c:IsType(TYPE_SYNCHRO,scard,sumtype,tp)
+end
+--Local No.1
+function s.filter(c)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsMonster()
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	g:Remove(s.codefilterchk,nil,e:GetHandler())
+	if c:IsFacedown() or #g<=0 then return end
+	repeat
+		local tc=g:GetFirst()
+		local code=tc:GetOriginalCode()
+		local cid=c:CopyEffect(code,RESET_EVENT+RESETS_STANDARD,1)
+		c:RegisterFlagEffect(code,RESET_EVENT+RESETS_STANDARD,0,0)
+		local e0=Effect.CreateEffect(c)
+		e0:SetCode(id)
+		e0:SetLabel(code)
+		e0:SetReset(RESET_EVENT+RESETS_STANDARD)
+		c:RegisterEffect(e0,true)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_ADJUST)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetLabel(cid)
+		e1:SetLabelObject(e0)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetOperation(s.resetop)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		c:RegisterEffect(e1,true)
+		g:Remove(s.codefilter,nil,code)
+	until #g<=0
+end
+function s.codefilter(c,code)
+	return c:IsOriginalCode(code) and c:IsAttribute(ATTRIBUTE_DARK)
+end
+function s.codefilterchk(c,sc)
+	return sc:GetFlagEffect(c:GetOriginalCode())>0
+end
+function s.resetop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	if not g:IsExists(s.codefilter,1,nil,e:GetLabelObject():GetLabel()) or c:IsDisabled() then
+		c:ResetEffect(e:GetLabel(),RESET_COPY)
+		c:ResetFlagEffect(e:GetLabelObject():GetLabel())
+	end
+end
+--Local No.2
+function s.spfilter(c,e,tp)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,1-tp)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(1-tp,LOCATION_MZONE,tp)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.spopp(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(1-tp,LOCATION_MZONE,tp)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,1-tp,false,false,POS_FACEUP_DEFENSE) 
+	end
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(1-tp,LOCATION_MZONE,tp)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil,e,tp):GetFirst()
+	if sc and Duel.SpecialSummon(sc,0,tp,1-tp,false,false,POS_FACEUP_DEFENSE)>0 then
+		--Negate its effects
+		sc:NegateEffects(e:GetHandler())
+	end
+end
