8da9dabf4ca52a2dc99aeb96153025885c22d832
Winterfer <feydersonrodriguez13@gmail.com>
2026-07-30 17:45:40 -0400

    new file upload

--- script/c889910.lua
+++ script/c889910.lua
@@ -4,12 +4,12 @@ function s.initial_effect(c)
 	-- Invocación por Sincronía: Exige 1 Cantante "Rosa" + 1 o más monstruos no Cantantes
 	Synchro.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,SET_ROSE),1,1,Synchro.NonTuner(nil),1,99)
 	
-	-- EFECTO ① ACTUALIZADO: Efecto Rápido que selecciona cartas igual a los materiales, niega y baraja
+	-- EFECTO ① DEFINITIVO: Efecto Rápido Libre que niega cartas y anula sus resoluciones en cadena antes de barajar
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_DISABLE+CATEGORY_TODECK)
-	e1:SetType(EFFECT_TYPE_QUICK_O) -- Transmutado a Efecto Rápido
-	e1:SetCode(EVENT_FREE_CHAIN)    -- Cadena Libre: Se activa en cualquier momento
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_QUICK_O) -- Efecto Rápido
+	e1:SetCode(EVENT_FREE_CHAIN)    -- Cadena Libre (Lo activas en cualquier momento que desees)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
@@ -22,8 +22,8 @@ function s.initial_effect(c)
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_TOEXTRA+CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_QUICK_O) -- Efecto Rápido
-	e3:SetCode(EVENT_FREE_CHAIN)    -- Cadena Libre
+	e3:SetType(EFFECT_TYPE_QUICK_O) 
+	e3:SetCode(EVENT_FREE_CHAIN)    
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1)
@@ -35,7 +35,7 @@ end
 s.listed_series={SET_ROSE}
 
 -- =========================================================================
--- ---        RESOLUCIÓN DEL EFECTO ① (DRENAJE Y LIMPIEZA RÁPIDA)       ---
+-- ---        RESOLUCIÓN DEL EFECTO ① (DRENAJE ABSOLUTO EN CADENAS)     ---
 -- =========================================================================
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SYNCHRO
@@ -47,33 +47,39 @@ function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local mg=c:GetMaterial()
 	local count=mg and #mg or 0
 	if chk==0 then return count>0 and Duel.IsExistingTarget(Card.IsAbleToDeck,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 	local g=Duel.SelectTarget(tp,Card.IsAbleToDeck,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,count,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,#g,0,0)
+	
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,g,#g,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
 end
 
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
 	local g=Duel.GetTargetCards(e)
 	if #g>0 then
 		local dg=Group.CreateGroup()
+		local chain_count=Duel.GetCurrentChain()
+		
 		for tc in aux.Next(g) do
-			if tc:IsFaceup() and not tc:IsImmuneToEffect(e) then
-				local e1=Effect.CreateEffect(c)
-				e1:SetType(EFFECT_TYPE_SINGLE)
-				e1:SetCode(EFFECT_DISABLE)
-				e1:SetReset(RESETS_STANDARD)
-				tc:RegisterEffect(e1)
-				local e2=Effect.CreateEffect(c)
-				e2:SetType(EFFECT_TYPE_SINGLE)
-				e2:SetCode(EFFECT_DISABLE_EFFECT)
-				e2:SetReset(RESETS_STANDARD)
-				tc:RegisterEffect(e2)
+			-- Recorre la cadena activa buscando si este objetivo inició un eslabón
+			for i=1,chain_count do
+				local te,tgp=Duel.GetChainInfo(i,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_PLAYER)
+				if tgp~=tp and te and te:GetHandler()==tc then
+					-- Niega estrictamente la activación/resolución de ese eslabón en la cadena actual
+					Duel.NegateActivation(i)
+				end
 			end
+			
+			-- Desvincula de forma segura cualquier otra cadena remota del objetivo
+			Duel.NegateRelatedChain(tc,RESET_TURN_SET) 
 			dg:AddCard(tc)
 		end
-		Duel.BreakEffect()
+		
+		-- Pausa estética reglamentaria de Konami
+		Duel.BreakEffect() 
+		
+		-- Envía las cartas seleccionadas de regreso al Deck completamente limpias
 		Duel.SendtoDeck(dg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
 	end
 end
@@ -108,3 +114,4 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
+
